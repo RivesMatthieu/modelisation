39f0cc492724a3ed6337b6194459d6ee1c1d8200
Matthieu <rivesmatthieu@gmail.com>
2020-02-28 19:31:10 +0100

    Add class and def

--- myapp.py
+++ myapp.py
@@ -7,3 +7,22 @@ list_column = []
 for x in clean_df:
     list_column.append(x)
 
+class App:  
+    def __init__(self,X, Y) :
+        self.X = X
+        self.Y = Y
+        
+    def model_Dorian(self, X, Y) :
+       logreg = LogisticRegression()
+       logreg.fit(self.X,self.Y)
+       print(logreg.predict(self.X))
+       print(logreg.score(self.X,self.Y))
+       X_train, X_test, Y_train, Y_test = train_test_split(self.X, self.Y, test_size=0.2)   
+       
+    def model_Lothaire(self) :
+        print('Modele de Lothaire')
+        
+    def model_Matthieu() :
+        print('Hello world !')
+        
+    model_Dorian(self, clean_df[['NB', 'a2']], clean_df['y'])
