579a1932faf7018674a0be105f22faefc54c3d41
Matthieu-uniweb <matthieu@uniweb-toulouse.fr>
2020-02-28 11:31:46 +0100

    Ignore Nombre de commande 0

--- myapp.py
+++ myapp.py
@@ -1,3 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv('dc_modelisation.csv', sep=';')
+df = pd.read_csv('./dc_modelisation.csv', sep=';')
+clean_df = df.loc[df["NB"] != 0 , :  ]
+list_column = []
+
+for x in clean_df:
+    list_column.append(x)
+
